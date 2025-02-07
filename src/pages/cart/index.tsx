0790c37c83a7661d68b1a3be9e71b90391a6df04
HamzaAlikundi <hamza.alee83@gmail.com>
2025-02-07 16:45:25 +0500

    add newslatter in cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import CartPage from '../../components/Cart'
+import Newsletter from '../../components/layout/Footer/NewsLatter'
 
 const Cart = () => {
     return (
-        <CartPage />
+        <>
+            <CartPage />
+            <Newsletter />
+        </>
     )
 }
 
